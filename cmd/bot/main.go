28fe0252511bfa5dcc49cbc83ecc8ead570bdd9d
vladimirovna261928-stack <vladimirovna261928@gmail.com>
2025-08-22 12:06:26 +0300

    add

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
-func main(){
-	fmt.Println("hello")
+	"github.com/joho/godotenv"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const token = "8237403924:AAH1Vhr0rAFgpaYbzkZ07exSszTWpivOqIE"
+
+func main() {
+	godotenv.Load()
+	
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil{
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		log.Printf("[%s]$%s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote:"+update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module bot
+
+go 1.24.5
+
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
